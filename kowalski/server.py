cafb78b82dec89e0fa6e36d36b62cc9b5afe7186
Dmitry Duev <dmitry.duev@gmail.com>
2018-11-11 12:49:22 -0800

    wip on jwt auth

--- kowalski/server.py
+++ kowalski/server.py
@@ -2,10 +2,13 @@ from aiohttp import web
 import jinja2
 import aiohttp_jinja2
 import json
+import jwt
 from motor.motor_asyncio import AsyncIOMotorClient
+import datetime
 
-# from kowalski.utils import utc_now, jd, radec_str2geojson
-from utils import utc_now, jd, radec_str2geojson, generate_password_hash
+# from kowalski.utils import utc_now, jd, radec_str2geojson, generate_password_hash, check_password_hash
+# from .utils import utc_now, jd, radec_str2geojson, generate_password_hash, check_password_hash
+from utils import utc_now, jd, radec_str2geojson, generate_password_hash, check_password_hash
 
 
 ''' load config and secrets '''
@@ -18,6 +21,12 @@ with open('/app/secrets.json') as sjson:
 for k in secrets:
     config[k].update(secrets.get(k, {}))
 # print(config)
+
+
+def json_response(body: dict, **kwargs):
+    kwargs['body'] = json.dumps(body or kwargs['body']).encode('utf-8')
+    kwargs['content_type'] = 'text/json'
+    return web.Response(**kwargs)
 
 
 async def init_db():
@@ -69,6 +78,51 @@ async def add_admin(_mongo):
 routes = web.RouteTableDef()
 
 
+@routes.post('/auth')
+async def auth(request):
+    post_data = await request.post()
+    # post_data = dict(post_data)
+    # print(post_data)
+
+    # must contain 'username' and 'password'
+
+    if ('username' not in post_data) or (len(post_data['username']) == 0):
+        return json_response({'message': 'Missing "username"'}, status=400)
+    if ('password' not in post_data) or (len(post_data['password']) == 0):
+        return json_response({'message': 'Missing "password"'}, status=400)
+
+    # # todo: check penquins version
+    # penquins_version = flask.request.json.get('penquins.__version__', None)
+    # if penquins_version is None or penquins_version not in config['misc']['supported_penquins_versions']:
+    #     return flask.jsonify({"msg": "Unsupported version of penquins. " +
+    #                                  "Please install the latest version + read current tutorial on Kowalski!"}), 400
+
+    username = str(post_data['username'])
+    password = str(post_data['password'])
+
+    try:
+        # user exists and passwords match?
+        select = await request.app['mongo'].users.find_one({'_id': username})
+        if check_password_hash(select['password'], password):
+            payload = {
+                'user_id': username,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(
+                    seconds=request.app['JWT']['JWT_EXP_DELTA_SECONDS'])
+            }
+            jwt_token = jwt.encode(payload,
+                                   request.app['JWT']['JWT_SECRET'],
+                                   request.app['JWT']['JWT_ALGORITHM'])
+
+            return json_response({'token': jwt_token.decode('utf-8')})
+
+        else:
+            return json_response({'message': 'Wrong credentials'}, status=400)
+
+    except Exception as e:
+        print(str(e))
+        return json_response({'message': 'Wrong credentials'}, status=400)
+
+
 @routes.get('/hw')
 async def hello(request):
     return web.Response(text="Hello, world")
@@ -91,10 +145,10 @@ async def app_factory(_config):
     :return:
     """
 
-    ''' init db if necessary '''
+    # init db if necessary
     await init_db()
 
-    ''' Database connection '''
+    # Database connection
     client = AsyncIOMotorClient(f"mongodb://{config['database']['user']}:{config['database']['pwd']}@" +
                                 f"{config['database']['host']}:{config['database']['port']}/{config['database']['db']}")
     mongo = client[config['database']['db']]
@@ -102,17 +156,26 @@ async def app_factory(_config):
     # add site admin if necessary
     await add_admin(mongo)
 
+    # init app
     app = web.Application()
 
-    async def close_mongo(app):
-        mongo.client.close()
-
+    # store mongo connection
     app['mongo'] = mongo
 
+    # graciously close mongo client on shutdown
+    async def close_mongo(app):
+        app['mongo'].client.close()
     app.on_cleanup.append(close_mongo)
 
+    # set up JWT for user authentication/authorization
+    app['JWT'] = {'JWT_SECRET': config['server']['JWT_SECRET_KEY'],
+                  'JWT_ALGORITHM': 'HS256',
+                  'JWT_EXP_DELTA_SECONDS': 30 * 86400 * 3}
+
+    # render templates with jinja2
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates'))
 
+    # route table
     # app.add_routes([web.get('/', hello)])
     app.add_routes(routes)
 
