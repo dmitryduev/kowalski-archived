8b6a6fe8df97cdcf3c8c9441e69385853030264e
Dmitry Duev <dmitry.duev@gmail.com>
2018-11-20 13:15:36 -0800

    wip: penquins

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='penquins', version='1.0.0', py_modules=['penquins'],
+      install_requires=['requests>=2.18.4',
+                        'numpy>=1.13.1']
+      )
+
