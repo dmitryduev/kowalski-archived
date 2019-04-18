d04ba8119105de9f7ca86f062939e715a491d1c4
Dmitry Duev <dmitry.duev@gmail.com>
2019-04-17 18:44:59 -0700

    wip: ingest CLU20190406

--- kowalski/dev/ingest_clu_20190406.py
+++ kowalski/dev/ingest_clu_20190406.py
@@ -163,26 +163,22 @@ if __name__ == '__main__':
     db[_collection].create_index([('coordinates.radec_geojson', '2dsphere'),
                                   ('_id', pymongo.ASCENDING)], background=True)
 
-    # number of records to insert
-    batch_size = 2048
-    batch_num = 1
-    documents = []
-
     clu_fits = '/_tmp/clu/CLU_20190406.hdf5'
 
     data = pd.read_hdf(clu_fits, key='data')
 
     for ii, dff in enumerate(yield_batch(data, num_batches=100)):
 
-        print(f'Processing batch # {ii+1}')
+        print(f'Processing chunk # {ii+1} of 100')
+
+        # number of records to insert
+        documents = []
 
         # if ii == 63267:
 
         # dff.rename(index=str, columns={'id': '_id'}, inplace=True)
         dff['_id'] = dff['cluid']
         batch = dff.to_dict(orient='records')
-
-        bad_doc_ind = []
 
         for ie, doc in enumerate(batch):
             try:
@@ -223,26 +219,14 @@ if __name__ == '__main__':
 
                 documents.append(doc)
 
-                # time.sleep(1)
-                # print(doc)
-                # input()
-
-                # insert batch, then flush
-                if len(documents) == batch_size:
-                    print(f'inserting batch #{batch_num}')
-                    insert_multiple_db_entries(db, _collection=_collection, _db_entries=documents)
-                    # flush:
-                    documents = []
-                    batch_num += 1
-
             except Exception as e:
                 traceback.print_exc()
                 print(e)
                 continue
 
-        # stuff left from the last file?
+        #
         if len(documents) > 0:
-            print(f'inserting batch #{batch_num}')
+            print(f'inserting chunk #{ii}')
             insert_multiple_db_entries(db, _collection=_collection, _db_entries=documents)
 
     print('All done')
