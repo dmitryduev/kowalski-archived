2a57a1465d1db8060f413cc5b919b954e972f157
Dmitry Duev <dmitry.duev@gmail.com>
2019-06-03 16:05:40 -0700

    wip: penquins: retry in case no connections available

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='penquins', version='1.0.0', py_modules=['penquins'],
+setup(name='penquins', version='1.0.1', py_modules=['penquins'],
       install_requires=['pymongo>=3.4.0',
                         'pytest>=3.3.0',
                         'requests>=2.18.4']
