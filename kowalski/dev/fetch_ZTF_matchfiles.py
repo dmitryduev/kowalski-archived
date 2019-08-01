369ea08c3cb4ed7efeca608642e2b8cd718911f6
Dmitry Duev <dmitry.duev@gmail.com>
2019-08-01 13:40:28 -0700

    wip: ZTF matchfiles 20190718

--- kowalski/dev/fetch_ZTF_matchfiles.py
+++ kowalski/dev/fetch_ZTF_matchfiles.py
@@ -81,5 +81,5 @@ if __name__ == '__main__':
     print(f'Downloading {n_matchfiles} matchfiles:')
 
     # download
-    with mp.Pool(processes=4) as p:
+    with mp.Pool(processes=3) as p:
         list(tqdm(p.imap(fetch_url, urls), total=n_matchfiles))
