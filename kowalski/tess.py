1038997f7ca244521f91be1b8826db675b754125
Dmitry Duev <dmitry.duev@gmail.com>
2019-07-31 18:29:38 -0700

    wip: dump TESS

--- kowalski/tess.py
+++ kowalski/tess.py
@@ -10,6 +10,7 @@ import tqdm
 from bson.json_util import dumps
 import subprocess
 import argparse
+import multiprocessing as mp
 
 
 ''' load config and secrets '''
@@ -56,6 +57,87 @@ def connect_to_db():
         raise ConnectionRefusedError
 
     return _client, _db
+
+
+def dump_tess_parallel(obsdate=None):
+
+    try:
+        # connect to MongoDB:
+        print(time_stamps(), 'Connecting to DB')
+        client, db = connect_to_db()
+        print(time_stamps(), 'Successfully connected')
+
+        datestr = datetime.datetime.utcnow().strftime('%Y%m%d') if obsdate is None else obsdate
+
+        path_date = os.path.join(config['path']['path_tess'], datestr)
+
+        # mkdir if necessary
+        if not os.path.exists(path_date):
+            os.makedirs(path_date)
+
+        jd = Time(datetime.datetime.strptime(datestr, '%Y%m%d')).jd
+
+        collection_alerts = 'ZTF_alerts'
+
+        # query = {'candidate.jd': {'$gt': jd, '$lt': jd + 1},
+        #          'candidate.programid': 1}
+        query = {'candidate.jd': {'$gt': jd, '$lt': jd + 1},
+                 'candidate.programid': 1,
+                 'candidate.programpi': 'TESS'}
+
+        # index name to use:
+        hint = 'candidate.jd_1_candidate.programid_1_candidate.programpi_1'
+
+        num_doc = db[collection_alerts].count_documents(query, hint=hint)
+        print(f'Alerts in TESS fields to compress: {num_doc}')
+
+        if num_doc > 0:
+
+            # fetch candid's:
+            cursor = db[collection_alerts].find(query, {'_id': 0, 'candid': 1}).hint(hint)  # .limit(3)
+
+            candids = []
+            for alert in tqdm.tqdm(cursor, total=num_doc):
+                candids.append(alert['candid'])
+
+            # cursor = db[collection_alerts].find(query).hint(hint)#.limit(3)
+            #
+            # # for alert in cursor.limit(1):
+            # for alert in tqdm.tqdm(cursor, total=num_doc):
+            #     # print(alert['candid'])
+            #     try:
+            #         with open(os.path.join(path_date, f"{alert['candid']}.json"), 'w') as f:
+            #             f.write(dumps(alert))
+            #     except Exception as e:
+            #         print(time_stamps(), str(e))
+
+            # # compress
+            # print(time_stamps(), 'Compressing')
+            # path_tarball_date = os.path.join(config['path']['path_tess'], f'{datestr}.tar.gz')
+            # subprocess.run(['/bin/tar', '-zcf', path_tarball_date, '-C', config['path']['path_tess'], datestr])
+            # print(time_stamps(), 'Finished compressing')
+            #
+            # # remove folder
+            # print(time_stamps(), f"Removing folder: {path_date}")
+            # subprocess.run(['rm', '-rf', path_date])
+            # print(time_stamps(), 'Done')
+            #
+            # # cp to ZTF's Google Cloud Storage with gsutil
+            # bucket_name = 'ztf-tess'
+            # print(time_stamps(), f'Uploading to gs://{bucket_name} bucket on Google Cloud')
+            # subprocess.run(['/usr/local/bin/gsutil', 'cp', path_tarball_date, f'gs://{bucket_name}/'])
+            # subprocess.run(['/usr/local/bin/gsutil', 'iam', 'ch', 'allUsers:objectViewer', f'gs://{bucket_name}'])
+            # print(time_stamps(), 'Done')
+
+        else:
+            print(time_stamps(), 'Nothing to do')
+
+        print(time_stamps(), 'Disconnecting from DB')
+        client.close()
+        print(time_stamps(), 'Successfully disconnected')
+
+    except Exception as e:
+        print(time_stamps(), str(e))
 
 
 def dump_tess(obsdate=None):
@@ -143,7 +225,8 @@ if __name__ == '__main__':
     if obs_date is None:
 
         # schedule.every(10).seconds.do(dump_tess)
-        schedule.every().day.at("15:30").do(dump_tess)
+        # schedule.every().day.at("15:30").do(dump_tess)
+        schedule.every().day.at("15:30").do(dump_tess_parallel)
 
         while True:
             schedule.run_pending()
@@ -151,4 +234,5 @@ if __name__ == '__main__':
 
     else:
         # run once for obs_date and exit
-        dump_tess(obs_date)
+        # dump_tess(obs_date)
+        dump_tess_parallel(obs_date)
