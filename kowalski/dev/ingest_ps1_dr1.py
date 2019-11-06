32a05cdab5b6ac1261accc2ca37072f4554e1769
Dmitry Duev <dmitry.duev@gmail.com>
2019-11-06 14:23:05 -0800

    wip: cats

--- kowalski/dev/ingest_ps1_dr1.py
+++ kowalski/dev/ingest_ps1_dr1.py
@@ -1,12 +1,136 @@
 import os
 import glob
 import pandas as pd
-import pymongo
 import json
 import argparse
-import traceback
 from concurrent.futures import ProcessPoolExecutor
-from ingest_utils import connect_to_db, deg2dms, deg2hms, insert_multiple_db_entries
+import datetime
+import pytz
+import pymongo
+import traceback
+from numba import jit
+import numpy as np
+
+
+def utc_now():
+    return datetime.datetime.now(pytz.utc)
+
+
+def connect_to_db(host, port, db, user, pwd):
+    """ Connect to the mongodb database
+
+    :return:
+    """
+    try:
+        # there's only one instance of DB, it's too big to be replicated
+        _client = pymongo.MongoClient(host=host, port=port)
+        # grab main database:
+        _db = _client[db]
+    except Exception as _e:
+        raise ConnectionRefusedError
+    try:
+        # authenticate
+        _db.authenticate(user, pwd)
+    except Exception as _e:
+        raise ConnectionRefusedError
+
+    return _client, _db
+
+
+def insert_db_entry(_db, _collection=None, _db_entry=None):
+    """
+        Insert a document _doc to collection _collection in DB.
+        It is monitored for timeout in case DB connection hangs for some reason
+    :param _collection:
+    :param _db_entry:
+    :return:
+    """
+    assert _collection is not None, 'Must specify collection'
+    assert _db_entry is not None, 'Must specify document'
+    try:
+        _db[_collection].insert_one(_db_entry)
+    except Exception as _e:
+        print('Error inserting {:s} into {:s}'.format(str(_db_entry['_id']), _collection))
+        traceback.print_exc()
+        print(_e)
+
+
+def insert_multiple_db_entries(_db, _collection=None, _db_entries=None, _verbose=False):
+    """
+        Insert a document _doc to collection _collection in DB.
+        It is monitored for timeout in case DB connection hangs for some reason
+    :param _db:
+    :param _collection:
+    :param _db_entries:
+    :param _verbose:
+    :return:
+    """
+    assert _collection is not None, 'Must specify collection'
+    assert _db_entries is not None, 'Must specify documents'
+    try:
+        _db[_collection].insert_many(_db_entries, ordered=False)
+    except pymongo.errors.BulkWriteError as bwe:
+        if _verbose:
+            print(bwe.details)
+    except Exception as _e:
+        if _verbose:
+            traceback.print_exc()
+            print(_e)
+
+
+@jit(forceobj=True)
+def deg2hms(x):
+    """Transform degrees to *hours:minutes:seconds* strings.
+
+    Parameters
+    ----------
+    x : float
+        The degree value c [0, 360) to be written as a sexagesimal string.
+
+    Returns
+    -------
+    out : str
+        The input angle written as a sexagesimal string, in the
+        form, hours:minutes:seconds.
+
+    """
+    assert 0.0 <= x < 360.0, 'Bad RA value in degrees'
+    # ac = Angle(x, unit='degree')
+    # hms = str(ac.to_string(unit='hour', sep=':', pad=True))
+    # print(str(hms))
+    _h = np.floor(x * 12.0 / 180.)
+    _m = np.floor((x * 12.0 / 180. - _h) * 60.0)
+    _s = ((x * 12.0 / 180. - _h) * 60.0 - _m) * 60.0
+    hms = '{:02.0f}:{:02.0f}:{:07.4f}'.format(_h, _m, _s)
+    # print(hms)
+    return hms
+
+
+@jit(forceobj=True)
+def deg2dms(x):
+    """Transform degrees to *degrees:arcminutes:arcseconds* strings.
+
+    Parameters
+    ----------
+    x : float
+        The degree value c [-90, 90] to be converted.
+
+    Returns
+    -------
+    out : str
+        The input angle as a string, written as degrees:minutes:seconds.
+
+    """
+    assert -90.0 <= x <= 90.0, 'Bad Dec value in degrees'
+    # ac = Angle(x, unit='degree')
+    # dms = str(ac.to_string(unit='degree', sep=':', pad=True))
+    # print(dms)
+    _d = np.floor(abs(x)) * np.sign(x)
+    _m = np.floor(np.abs(x - _d) * 60.0)
+    _s = np.abs(np.abs(x - _d) * 60.0 - _m) * 60.0
+    dms = '{:02.0f}:{:02.0f}:{:06.3f}'.format(_d, _m, _s)
+    # print(dms)
+    return dms
 
 
 ''' load config and secrets '''
@@ -98,10 +222,11 @@ def process_file(_file, _collection, _batch_size=2048, verbose=False, _dry_run=F
                     ('yMeanKronMag', float),
                     ('yMeanKronMagErr', float),
                     ('yFlags', int)]
+    colnames = [c[0] for c in column_names]
 
     print(f'processing {_file}')
 
-    for ii, dff in enumerate(pd.read_csv(_file, chunksize=_batch_size)):
+    for ii, dff in enumerate(pd.read_csv(_file, chunksize=_batch_size, header=None, names=colnames)):
 
         print(f'{_file}: processing batch # {ii + 1}')
 
@@ -112,27 +237,10 @@ def process_file(_file, _collection, _batch_size=2048, verbose=False, _dry_run=F
 
         for ie, doc in enumerate(batch):
             try:
-                doc['_id'] = doc['objID']
-                doc.pop('objID', None)
-
-                # convert types and pop empty fields
-                for col_name, col_type in column_names:
-                    try:
-                        if doc[col_name] == 'NOT_AVAILABLE':
-                            # continue
-                            doc.pop(col_name, None)
-                        elif doc[col_name] == 'false':
-                            doc[col_name] = False
-                        elif doc[col_name] == 'true':
-                            doc[col_name] = True
-                        elif len(doc[col_name]) == 0:
-                            # doc[col_name] = None
-                            doc.pop(col_name, None)
-                        else:
-                            doc[col_name] = col_type(doc[col_name])
-                    except Exception as e:
-                        traceback.print_exc()
-                        print(e)
+                keys = list(doc.keys())
+                for key in keys:
+                    if (doc[key] == 'NOT_AVAILABLE') or (doc[key] == -999.0):
+                        doc.pop(key, None)
 
                 # GeoJSON for 2D indexing
                 doc['coordinates'] = dict()
