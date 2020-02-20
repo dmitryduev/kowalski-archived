b7b3152ac351988b112cce1fe3727c309eab8c17
Dmitry Duev <dmitry.duev@gmail.com>
2020-02-20 14:48:53 -0800

    wip: ingest GALEX

--- kowalski/dev/ingest_galex.py
+++ kowalski/dev/ingest_galex.py
@@ -97,7 +97,7 @@ def insert_multiple_db_entries(_db, _collection=None, _db_entries=None, _verbose
             print(_e)
 
 
-@jit
+@jit(forceobj=True)
 def deg2hms(x):
     """Transform degrees to *hours:minutes:seconds* strings.
 
@@ -125,7 +125,7 @@ def deg2hms(x):
     return hms
 
 
-@jit
+@jit(forceobj=True)
 def deg2dms(x):
     """Transform degrees to *degrees:arcminutes:arcseconds* strings.
 
